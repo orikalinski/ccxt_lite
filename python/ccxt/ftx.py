bbd54c1c26ff3a66c2176635d0fe5668065c2c6b
Ori Kalinski <orikalinski@gmail.com>
2022-06-21 10:01:06 +0300

    changing the ftx url

--- python/ccxt/ftx.py
+++ python/ccxt/ftx.py
@@ -42,7 +42,7 @@ class ftx(Exchange):
             'hostname': 'ftx.com',  # or ftx.us
             'urls': {
                 'logo': 'https://user-images.githubusercontent.com/1294454/67149189-df896480-f2b0-11e9-8816-41593e17f9ec.jpg',
-                'www': 'https://ftx.com',
+                'www': 'https://awsapi.ftx.com',
                 'api': {
                     'public': 'https://{hostname}',
                     'private': 'https://{hostname}',
