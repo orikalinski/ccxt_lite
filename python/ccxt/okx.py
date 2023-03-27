1b5148497071fcab62688b127a9fcd67cad2e961
Ori Kalinski <ori@cornix.io>
2023-03-27 17:06:13 +0300

    multiplying the filled and amount by contract size inside okx

--- python/ccxt/okx.py
+++ python/ccxt/okx.py
@@ -2348,6 +2348,7 @@ class okx(Exchange):
         #         "uly": "BTC-USDT"
         #     }
         #
+        contractSize = market["contractSize"]
         id = self.safe_string_2(order, 'algoId', 'ordId')
         timestamp = self.safe_integer(order, 'cTime')
         lastTradeTimestamp = self.safe_integer(order, 'fillTime')
@@ -2366,7 +2367,7 @@ class okx(Exchange):
             type = 'limit'
         marketId = self.safe_string(order, 'instId')
         symbol = self.safe_symbol(marketId, market, '-')
-        filled = self.safe_float(order, 'accFillSz', default_value=0.)
+        filled = self.safe_float(order, 'accFillSz', default_value=0.) * contractSize
         price = self.safe_float_2(order, 'px', 'ordPx')
         average = self.safe_float(order, 'avgPx')
         status = self.parse_order_status(self.safe_string(order, 'state'))
@@ -2385,7 +2386,7 @@ class okx(Exchange):
             cost = self.safe_float(order, 'sz')
         else:
             # "sz" refers to the trade currency amount
-            amount = self.safe_float(order, 'sz')
+            amount = self.safe_float(order, 'sz') * contractSize
         fee = None
         if fee_cost is not None:
             feeCurrencyId = self.safe_string(order, 'feeCcy')
